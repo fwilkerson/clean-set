06c20271f5921346193b4669203bc5d2f039bfe2
fwilkerson <frank.wilkerson@codercamps.com>
2018-06-21 10:56:03 -0700

    handle accessing a specific index of an array

--- dist/clean-set.js
+++ dist/clean-set.js
@@ -1,2 +1,2 @@
-function r(r,n){for(var t in n=n||{},r)n[t]=r[t];return n}module.exports=function(n,t,l){t.split&&(t=t.split("."));for(var o=r(n),e=o,i=0,u=t.length;i<u;i++)e=e[t[i]]=i===u-1?l.call?l(e[t[i]]):l:r(e[t[i]]);return o};
+function r(r){var n=r&&r.pop?[]:{};for(var t in r)n[t]=r[t];return n}module.exports=function(n,t,o){t.split&&(t=t.split("."));for(var l=r(n),e=l,i=0,p=t.length;i<p;i++)e=e[t[i]]=i===p-1?o.call?o(e[t[i]]):o:r(e[t[i]]);return l};
 //# sourceMappingURL=clean-set.js.map
